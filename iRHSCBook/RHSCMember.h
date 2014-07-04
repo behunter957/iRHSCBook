1f4d22e654015a6837f2f4209762c1cf8d869fcb
Bruce Hunter <b_hunter@rogers.com>
2014-07-04 19:03:06 -0400

    test again

--- iRHSCBook/RHSCMember.h
+++ iRHSCBook/RHSCMember.h
@@ -10,6 +10,8 @@
 
 @interface RHSCMember : NSObject
 
+//test change
+
 @property (nonatomic, strong, readonly) NSString *name;
 @property (nonatomic, strong, readonly) NSString *email;
 @property (nonatomic, strong, readonly) NSString *phone1;
