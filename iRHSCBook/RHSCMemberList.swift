a2011d597e763dcd76685ea2aa402a1c7e5512b0
Bruce Hunter <b_hunter@rogers.com>
2015-10-25 18:21:20 -0400

    swift replacement

---
+++ iRHSCBook/RHSCMemberList.swift
@@ -0,0 +1,76 @@
+//
+//  RHSCMemberList.swift
+//  iRHSCBook
+//
+//  Created by Bruce Hunter on 2015-10-25.
+//  Copyright © 2015 Richmond Hill Squash Club. All rights reserved.
+//
+
+import Foundation
+
+@objc class RHSCMemberList : NSObject {
+
+    var memberList : Array<RHSCMember> = Array<RHSCMember>()
+    var TBD : RHSCMember? = nil
+    var GUEST : RHSCMember? = nil
+    var isLoaded : Bool = false
+    
+    func loadFromJSON(fromServer server :RHSCServer) {
+    // Create a NSURLRequest with the given URL
+        let logonURL : String = "Reserve20/IOSMemberListJSON.php"
+        let target = NSURL(string:logonURL, relativeToURL:server)
+        let request = NSURLRequest(URL:target!,
+                cachePolicy:NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData,
+                timeoutInterval:30.0)
+    // Get the data
+        let response:AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+        let error: AutoreleasingUnsafeMutablePointer<NSErrorPointer?> = nil
+        // Sending Synchronous request using NSURLConnection
+        do {
+            let responseData = try NSURLConnection.sendSynchronousRequest(request,returningResponse: response) as NSData
+            if (error == nil) {
+                loadFromData(responseData)
+            }
+        } catch {
+            
+        }
+    }
+    
+    func loadFromData(data : NSData) {
+    // Now create a NSDictionary from the JSON data
+        do {
+            let jsonDictionary: NSDictionary = try NSJSONSerialization.JSONObjectWithData(data,options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+    
+            // Get an array of dictionaries with the key "locations"
+            let array : Array<NSDictionary> = jsonDictionary["members"]! as! Array<NSDictionary>
+            // Iterate through the array of dictionaries
+            for dict in array {
+                // Create a new Location object for each one and initialise it with information in the dictionary
+                let member = RHSCMember(fromJSONDictionary:dict )
+                // Add the Location object to the array
+                memberList.append(member)
+            }
+            isLoaded = (self.memberList.count != 0);
+            memberList.append(RHSCMember(fromName: "TBD", fromType: "Active"))
+            memberList.append(RHSCMember(fromName: "GUEST", fromType: "Active"))
+        } catch {
+            
+        }
+    }
+    
+    func find(member name : String) -> RHSCMember? {
+        for tst in memberList {
+            if tst.name == name {
+                return tst;
+            }
+        }
+        return nil;
+    }
+    
+    func loadedSuccessfully() -> Bool
+    {
+        return isLoaded;
+    }
+    
+    
+}
