cfa4e5424a8028b1b0a5d1823b4ac4f9aaa0a865
Bruce Hunter <b_hunter@rogers.com>
2015-10-25 18:21:38 -0400

    swift replacement

---
+++ iRHSCBook/RHSCServer.swift
@@ -0,0 +1,13 @@
+//
+//  RHSCServer.swift
+//  iRHSCBook
+//
+//  Created by Bruce Hunter on 2015-10-25.
+//  Copyright © 2015 Richmond Hill Squash Club. All rights reserved.
+//
+
+import Foundation
+
+@objc class RHSCServer : NSURL {
+    
+}
