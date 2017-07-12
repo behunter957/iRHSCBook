8b77f4f8476ece2986b4b68218f497b250919257
Bruce Hunter <b_hunter@rogers.com>
2017-07-12 17:08:04 -0400

    more logon

--- iRHSCBook/RHSCLogonViewController.swift
+++ iRHSCBook/RHSCLogonViewController.swift
@@ -21,37 +21,65 @@ class RHSCLogonViewController : UIViewController, UITableViewDataSource, UITable
     
     var delegate : AnyObject? = nil
     
+    var currentUser : RHSCUser? = nil
+    var server : RHSCServer? = nil
     var successAlert : UIAlertController? = nil
     var errorAlert : UIAlertController? = nil
     
     override func viewDidLoad() {
         
-        //        self.tableView.accessibilityIdentifier = "BookCourt"
-        let dateFormat = DateFormatter()
-        dateFormat.dateFormat = "EEE, MMM d"
-        let timeFormat = DateFormatter()
-        timeFormat.dateFormat = "h:mm a"
-        // create the cells
-        r0 = formTable.dequeueReusableCell(withIdentifier: "UserID Cell") as? RHSCUserIDTableViewCell
-        if (r0 == nil) {
-            let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
-            r0 = nib?[0] as? RHSCUserIDTableViewCell
+        let defaults = UserDefaults.standard
+        let userid = defaults.string(forKey: "RHSCUserID") ?? "ChangeMe"
+        defaults.set(userid, forKey: "RHSCUserID")
+        let passwd = defaults.string(forKey: "RHSCPassword") ?? "ChangeMe"
+        defaults.set(passwd, forKey: "RHSCPassword")
+        let srvrname = defaults.string(forKey: "RHSCServerURL") ?? "http://www.rhsquashclub.com"
+        defaults.set(srvrname, forKey: "RHSCServerURL")
+
+        let networkReachability = Reachability()!
+        let networkStatus = networkReachability.currentReachabilityStatus
+        if (networkStatus == Reachability.NetworkStatus.notReachable) {
+            self.view.isUserInteractionEnabled = false
+            // if not found then logon failes
+            self.errorAlert = UIAlertController(title: "No Internet Connection",
+                                                message: "Close the application and try later.", preferredStyle: .alert)
+            DispatchQueue.global().async(execute: {
+                // do some task
+                DispatchQueue.main.async(execute: {
+                    self.present(self.errorAlert!, animated: true, completion: nil)
+                })
+            })
+        } else {
+            // try to logon with the defaults
+            self.currentUser = RHSCUser(forUserid: userid, forPassword: passwd)
+            self.server = RHSCServer(string: "", relativeTo: URL(string: srvrname))
+            if self.currentUser!.validate(fromServer: server!) {
+                // valid credentials - dismiss dialog and go to tab controller
+            } else {
+                // invalid credentials - continue with dialog
+                
+                // create the cells
+                r0 = formTable.dequeueReusableCell(withIdentifier: "UserID Cell") as? RHSCUserIDTableViewCell
+                if (r0 == nil) {
+                    let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
+                    r0 = nib?[0] as? RHSCUserIDTableViewCell
+                }
+                r1 = formTable.dequeueReusableCell(withIdentifier: "Password Cell") as? RHSCPasswordTableViewCell
+                if (r1 == nil) {
+                    let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
+                    r1 = nib?[0] as? RHSCPasswordTableViewCell
+                }
+                r2 = formTable.dequeueReusableCell(withIdentifier: "Logon Cell") as? RHSCButtonTableViewCell
+                if (r2 == nil) {
+                    let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
+                    r2 = nib?[3] as? RHSCButtonTableViewCell
+                }
+                cells = [r0, r1, r2]
+                for cell in cells {
+                    cell?.selectionStyle = .none
+                }
+            }
         }
-        r1 = formTable.dequeueReusableCell(withIdentifier: "Password Cell") as? RHSCPasswordTableViewCell
-        if (r1 == nil) {
-            let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
-            r1 = nib?[0] as? RHSCPasswordTableViewCell
-        }
-        r2 = formTable.dequeueReusableCell(withIdentifier: "Logon Cell") as? RHSCButtonTableViewCell
-        if (r2 == nil) {
-            let nib = Bundle.main.loadNibNamed("RHSCBookCourtTableViewCell", owner: self, options: nil)
-            r2 = nib?[3] as? RHSCButtonTableViewCell
-        }
-        cells = [r0, r1, r2]
-        for sect in cells {
-            sect?.selectionStyle = .none
-        }
-        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -90,6 +118,25 @@ class RHSCLogonViewController : UIViewController, UITableViewDataSource, UITable
     }
     
     @IBAction func logon() {
+        // try to logon with the defaults
+        self.currentUser = RHSCUser(forUserid: (r0?.textField.text)!, forPassword: (r1?.textField.text)!)
+        if self.currentUser!.validate(fromServer: server!) {
+            // valid credentials - save defaults, dismiss dialog and go to tab controller
+        } else {
+            // invalid credentials - put up error continue with dialog
+            self.errorAlert = UIAlertController(title: "Logon Failed",
+                                                message: "Please check settings and provide a valid userid and password.", preferredStyle: .alert)
+            DispatchQueue.global().async(execute: {
+                DispatchQueue.main.async(execute: {
+                    self.present(self.errorAlert!, animated: true, completion: nil)
+                    let delay = 2.0 * Double(NSEC_PER_SEC)
+                    let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+                    DispatchQueue.main.asyncAfter(deadline: time, execute: {
+                        self.errorAlert!.dismiss(animated: true, completion: nil)
+                    })
+                })
+            })
+        }
     }
     
 }
